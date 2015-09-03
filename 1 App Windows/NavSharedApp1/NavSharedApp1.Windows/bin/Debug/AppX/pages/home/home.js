21e36edcdf75d3510c4c15f2411bce4d8a4c198b
HenriqueVarellaEhrenfried <h.v.ehrenfried@hotmail.com>
2015-09-03 20:40:40 -0300

    First Windows project
    Study about create a Windows app using HTML and Javascript

---
+++ 1 App Windows/NavSharedApp1/NavSharedApp1.Windows/bin/Debug/AppX/pages/home/home.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+        }
+    });
+
+})();
+var itemArray = [
+        { title: "Java", text: "", picture: "../image/java.jpg" },
+        { title: "C", text: "", picture: "../image/C.png" },
+        { title: "C++", text: "", picture: "../image/cpp.png" },
+        { title: "Python", text: "", picture: "../image/Python.png" },
+        { title: "C#", text: "", picture: "../image/csharp.png" },
+        { title: "R", text: "", picture: "../image/R.png" },
+        { title: "PHP", text: "", picture: "../image/php.jpg" },
+        { title: "JavaScript", text: "", picture: "../image/Javascript.png" },
+        { title: "Ruby", text: "", picture: "../image/ruby.png" },
+        { title: "Matlab", text: "", picture: "../image/Matlab.png" },
+];
+
+var items = [];
+
+// Generate 160 items
+for (var i = 0; i <= 0; i++) {
+    itemArray.forEach(function (item) {
+        items.push(item);
+    });
+}
+
+
+WinJS.Namespace.define("Sample.ListView", {
+    data: new WinJS.Binding.List(items)
+});
+WinJS.UI.processAll();
+function solverProblem(eventInfo) {
+    window.location.href = "www.google.com";
+}
